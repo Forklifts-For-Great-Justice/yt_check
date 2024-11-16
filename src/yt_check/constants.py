f28fc66bd0ecbed3beb946cf9291a9dcdeed3f35
Jon Heise <j.heise@gmail.com>
2024-11-15 17:48:36 -0800

    adding new channels and updating to match pylint

--- src/yt_check/constants.py
+++ src/yt_check/constants.py
@@ -1,14 +1,22 @@
+""" Constants used by yt_check"""
+
 from enum import StrEnum
 
 
 class Channels(StrEnum):
-    dragonforce = ("UCIUA7-MNA9NyrpimQo7Dw8g",)
-    skeletor = ("UCOYYn-7CTVA1jFtmYOLr8Qw",)
+    """Channels monitored by yt_check"""
+
+    DRAGONFORCE = "UCIUA7-MNA9NyrpimQo7Dw8g"
+    SKELETOR = "UCOYYn-7CTVA1jFtmYOLr8Qw"
+    BABYMETAL = "UC33_tIj4m1_XaqfFcomShvw"
+    SABATON = "UCjQhd1APsd5NQhiVZV7GYzg"
 
 
 class Notifications(StrEnum):
-    one = "Fools! New videos have arrived. Watch them, if you dare!"
-    two = "Behold, mortals! A feast of new videos awaits. Feast your eyes upon them!"
-    three = "New videos have been unleashed upon the world. Let the chaos begin!"
-    four = "You boob! There are new videos to watch. Don't make me say it again!"
-    five = "Another day, another batch of videos. Enjoy, if you can."
+    """Notifications used by yt_check"""
+
+    ONE = "Fools! New videos have arrived. Watch them, if you dare!"
+    TWO = "Behold, mortals! A feast of new videos awaits. Feast your eyes upon them!"
+    THREE = "New videos have been unleashed upon the world. Let the chaos begin!"
+    FOUR = "You boob! There are new videos to watch. Don't make me say it again!"
+    FIVE = "Another day, another batch of videos. Enjoy, if you can."
